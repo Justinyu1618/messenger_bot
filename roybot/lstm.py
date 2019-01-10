7bd30521713a08c801ee50cab00f8aa9cdf8b1a1
Justin Yu <justinyu1618@gmail.com>
2019-01-10 04:06:56 -0500

    merp

--- roybot/lstm.py
+++ roybot/lstm.py
@@ -45,7 +45,7 @@ from __future__ import print_function
 from keras.models import Model, load_model
 from keras.layers import Input, LSTM, Dense
 import numpy as np
-
+import sys
 
 batch_size = 64  # Batch size for training.
 epochs = 100  # Number of epochs to train for.
